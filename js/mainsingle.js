ab2a4402fb054e4a8f868cad6df5d147f135ca40
ALSpringer <aaronlspringer@gmail.com>
2013-06-09 15:05:14 -0400

    Added single threaded counterpart and fixed weird commit error from last

--- js/mainsingle.js
+++ js/mainsingle.js
@@ -3,29 +3,20 @@ $(document).on('pageinit', '#startpage', function () {
   var startTime = 0,
       workers = [],
       first = true,
-      count = 0;
+      count = 0,
+      page = '<div data-role="page" id="thepage" class="simplepage"><div data-role="header" data-position="fixed" data-tap-toggle="false"><h1>This is a simple header</h1></div><div data-role="content"><ul data-role="listview" data-inset="true">';
 
   $.get('http://search.twitter.com/search.json?q=balling&rpp=20', function (data) {
-    startTime = Date.now();
-    for (var i = 0; i < NUM_WORKERS; i++) {
-      workers[i] = new Worker('js/createSimplePage.js');
-    }
-    for (var k = 0; k < NUM_WORKERS; k++) {
-      workers[k].postMessage(data);
-    }
     var body = $('body');
-    for (var j = 0; j < NUM_WORKERS; j++) {
-      workers[j].addEventListener('message', function (e) {
-        body.append(e.data);
-        if (count === 99) {
-          console.log((Date.now() - startTime));
-        }
-        count++;
-        if (first) {
-          $.mobile.changePage('#thepage');
-          first = false;
-        }
-      });
+    startTime = Date.now();
+    for (var i = 0; i < NUM_PAGES; i++) {
+      console.log("Doing " + i);
+      for (var j = 0; j < data.results.length; j++) {
+        page += '<li><img src="' + data.results[j].profile_image_url + '"><h2>' + data.results[j].from_user_name + '</h2><p>' + data.results[j].text + '</p><p class="ui-li-aside">' + data.results[j].created_at + '</p></li>';
+      }
+      page += '</ul></div></div>';
+      body.append(page);
     }
+    console.log((Date.now() - startTime));
   });
 });
